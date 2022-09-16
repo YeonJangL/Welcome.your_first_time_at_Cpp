350d8f02d3013336b0d590b17a8dbe40086ad363
YeonjangL <lej29331814@gmail.com>
2022-09-16 14:19:25 +0900

    Chapter01

---
+++ 58p_07.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int speed = 60;
+    int time = 2.5;
+    int destance;
+    destance = speed / time;
+    cout << "이동한 거리는 " << destance << "입니다." << endl;
+    return 0;
+}
